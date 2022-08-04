dea46a4e4b1dd06fc9466bb5711249b70defdd95
sinwan.meynaud <sinwan.meynaud@cbainfo.fr>
2022-08-04 12:26:18 +0200

    image optimisation + seo

---
+++ pages/galery.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Image from 'next/image'
+import img1 from '../public/assets/img1.jpg'
+import img2 from '../public/assets/img2.jpg'
+import img3 from '../public/assets/img3.jpg'
+
+
+export default function galery() {
+  return (
+    <>
+    <Image
+    layout='responsive' placeholder='blur'
+    src={img1}
+    width="16"
+    height="9"
+    />
+    <Image
+    layout='responsive' placeholder='blur'
+    src={img2}
+    width="3857"
+    height="2571"
+    />
+    <Image
+    layout='responsive' placeholder='blur'
+    src={img3}
+    width="4725"
+    height="3150"
+    />
+      {/* <img src="/assets/img1.jpg" alt="" />
+      <img src="/assets/img2.jpg" alt="" />
+      <img src="/assets/img3.jpg" alt="" /> */}
+    </>
+  )
+}
+
+/**
+ * 4.9 Mb -> 1 Mb
+ * 1.1 Mb -> 330 Kb
+ * 912 Kb -> 188 kB
+ */
